d315c05ebef3eca0b16a4a88f9f712159645ca01
Daniel Jaramillo <jaramo92@gmail.com>
2021-11-25 13:57:58 -0500

    Layout desarrollo, no merge

--- app/src/main/java/com/example/learningtoolbox/activities/MainActivity.kt
+++ app/src/main/java/com/example/learningtoolbox/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.learningtoolbox
+package com.example.learningtoolbox.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.learningtoolbox.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
