69ac08935d1fa7b554cb328b923ac4edc6c29698
Daniel Jaramillo <jaramo92@gmail.com>
2021-12-02 13:10:13 -0500

    Coneccion de layouts con navigate

--- app/src/main/java/com/example/learningtoolbox/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/learningtoolbox/view/ui/activities/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.learningtoolbox.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.learningtoolbox.R
+import com.example.learningtoolbox.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+    }
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnv_menu).setupWithNavController(navController)
     }
 }
